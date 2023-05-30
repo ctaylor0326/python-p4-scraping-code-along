c6a8f63ae93664a2bf5adf4ccbe1d4c77f00c15f
CahenTaylor <cahentaylor@gmail.com>
2023-05-30 10:16:44 -0600

    sldfkj

--- lib/Scraper.py
+++ lib/Scraper.py
@@ -1,8 +1,24 @@
-from bs4 import BeautifulSoup
+
+from bs4 import BeautifulSoup 
 import requests
-from Course import Course
 import ipdb
+import Course
+import json
+
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+
+soup = BeautifulSoup(html_text, 'html.parser')
+jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+for job in jobs:
+    company_name = job.find('h3', class_="joblist-comp-name").text.strip()
+    skills = job.find('span', class_="srp-skills").text.replace(' ','')
+    published_date = job.find('span', class_ = 'sim-posted').span.text
+    print(f'''
+    Company Name: {company_name}
+    Required Skills: {skills}
+    Published Date: {published_date}''')
+    print('')
+    
 
 
-class Scraper:
-    pass
+
--- lib/testing/course_test.py
+++ lib/testing/course_test.py
@@ -7,15 +7,22 @@ class Test_Course:
 
     def test_title_instance_variable(self):
         "Test setting and getting for title"
-        course = Course(title="Programming Robots for Outer Space")
+        course = Course(title="Programming Robots for Outer Space", schedule="Full-Time", description="Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
         assert(course.title == "Programming Robots for Outer Space")
+        assert(course.schedule == "Full-Time")
+        assert(course.description == "Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
 
     def test_schedule_instance_variable(self):
         "Test setting and getting for schedule"
-        course = Course(schedule="Full-Time")
+        course = Course(title="Programming Robots for Outer Space", schedule="Full-Time", description="Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
+        assert(course.title == "Programming Robots for Outer Space")
         assert(course.schedule == "Full-Time")
+        assert(course.description == "Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
+
 
     def test_description_instance_variable(self):
         "Test setting and getting for description"
-        course = Course(description="Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
+        course = Course(title="Programming Robots for Outer Space", schedule="Full-Time", description="Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
+        assert(course.title == "Programming Robots for Outer Space")
+        assert(course.schedule == "Full-Time")
         assert(course.description == "Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
